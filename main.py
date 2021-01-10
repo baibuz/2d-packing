ef7b720ce41d09d9683f3cc932256417a69952e2
Ekaterina Baibuz <ekaterina.baibuz@gmail.com>
2021-01-10 16:10:12 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# This is the main python script for 2D packing problem with Metropolis Monte Carlo algorithm
+# by E. Baibuz
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
